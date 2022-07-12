e8866dc6b8a646ec80ba3ab2cc35ef03b5cd2ae0
TakahashiJumpei <b1014098@gmail.com>
2022-07-12 14:55:37 +0900

    修正｜トップ画面でスレッド一覧が０件の場合の処理を修正

--- src/app/Http/Controllers/TopController.php
+++ src/app/Http/Controllers/TopController.php
@@ -15,6 +15,9 @@ class TopController extends Controller
 
     //DBから最近作成された最新１０件のスレッドを最新順に取得する
     $recently_created_threads = Thread::orderBy('id', 'DESC')->take(10)->get();
+    if ($recently_created_threads->isEmpty()) {
+      $recently_created_threads = null;
+    }
 
     //DBからスレッドを最近コメントされた順にソートして取得
     //まずコメントテーブルからコメントを外部キーであるスレッドテーブルIDが重複しないように１０件取得する。
@@ -23,13 +26,18 @@ class TopController extends Controller
       $query->select(DB::raw('MAX(id) As id'))->from('comments')->groupBy('thread_id');
     })->get();
     Log::info('$recently_comments', [$recently_comments]);
-    $thread_ids = array();
-    foreach ($recently_comments as $recently_comment) {
-      Log::info('$recently_comment->thread_id', [$recently_comment->thread_id]);
-      $thread_ids[] = $recently_comment->thread_id;
+    if ($recently_comments->isEmpty()) {
+      $recently_comments = null;
+      $recently_commented_threads = null;
+    } else {
+      $thread_ids = array();
+      foreach ($recently_comments as $recently_comment) {
+        Log::info('$recently_comment->thread_id', [$recently_comment->thread_id]);
+        $thread_ids[] = $recently_comment->thread_id;
+      }
+      $recently_commented_threads = Thread::whereIn('id', $thread_ids)->orderByRaw('FIELD(id, ' . implode(',', $thread_ids) . ')')->take(10)->get();
+      Log::info('$recently_commented_threads', [$recently_commented_threads]);
     }
-    $recently_commented_threads = Thread::whereIn('id', $thread_ids)->orderByRaw('FIELD(id, '.implode(',', $thread_ids).')')->take(10)->get();
-    Log::info('$recently_commented_threads', [$recently_commented_threads]);
 
     return view('top', compact('recently_created_threads', 'recently_commented_threads'));
   }
