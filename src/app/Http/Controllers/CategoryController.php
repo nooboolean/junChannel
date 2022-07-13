1e9508825a9deab08160cd7ea95a7a10634d9663
TakahashiJumpei <b1014098@gmail.com>
2022-07-13 23:33:32 +0900

    実装｜属するスレッドが無い場合の表示制御の実装

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -24,6 +24,7 @@ class CategoryController extends Controller
       $categories = null;
     }
     Log::info('$categories', [$categories]);
+    //dd($categories);
 
     //DBから指定したカテゴリに属するスレッドを最近コメントされた順にソートして取得
     //コメントはスレッドに属する
@@ -34,40 +35,43 @@ class CategoryController extends Controller
     Log::info('$threads_in_this_category', [$threads_in_this_category]);
     //dd($threads_in_this_category);
 
-    //まずコメントテーブルからコメントを外部キーであるスレッドテーブルIDが重複しないように１０件取得する。
-    //スレッドテーブルが最新のコメント投稿日時順に10種類取得できているので、スレッドをその順番で１０件取得できる。
-    $recently_comments = Comment::orderBy('id', 'DESC')->whereIn('id', function ($query) {
-      $query->select(DB::raw('MAX(id) As id'))->from('comments')->groupBy('thread_id');
-    })->get();
-    Log::info('$recently_comments', [$recently_comments]);
-    if ($recently_comments->isEmpty()) {
-      $recently_comments = null;
+    if ($threads_in_this_category->isEmpty()) {
       $recently_commented_threads = null;
     } else {
-      //上記で取得したコメントIDが属するスレッドのみを取得する
-      $thread_ids = array();
-      foreach ($recently_comments as $recently_comment) {
-        Log::info('$recently_comment->thread_id', [$recently_comment->thread_id]);
-        $thread_ids[] = $recently_comment->thread_id;
-      }
-      Log::info('$thread_ids[]', $thread_ids);
-      Log::info('aaa');
+      //まずコメントテーブルからコメントを外部キーであるスレッドテーブルIDが重複しないように１０件取得する。
+      //スレッドテーブルが最新のコメント投稿日時順に10種類取得できているので、スレッドをその順番で１０件取得できる。
+      $recently_comments = Comment::orderBy('id', 'DESC')->whereIn('id', function ($query) {
+        $query->select(DB::raw('MAX(id) As id'))->from('comments')->groupBy('thread_id');
+      })->get();
+      Log::info('$recently_comments', [$recently_comments]);
+      if ($recently_comments->isEmpty()) {
+        $recently_comments = null;
+        $recently_commented_threads = null;
+      } else {
+        //上記で取得したコメントIDが属するスレッドのみを取得する
+        $thread_ids = array();
+        foreach ($recently_comments as $recently_comment) {
+          Log::info('$recently_comment->thread_id', [$recently_comment->thread_id]);
+          $thread_ids[] = $recently_comment->thread_id;
+        }
+        Log::info('$thread_ids[]', $thread_ids);
 
-      $recently_commented_thread_ids_in_this_category = array();
-      foreach ($thread_ids as $thread_id) {
-        Log::info('$thread_id', [$thread_id]);
-        foreach ($threads_in_this_category as $thread_in_this_category) {
-          Log::info('$thread_in_this_category', [$thread_in_this_category]);
-          if($thread_id == $thread_in_this_category->id){
-            $recently_commented_thread_ids_in_this_category[] = $thread_id;
-            break;
+        $recently_commented_thread_ids_in_this_category = array();
+        foreach ($thread_ids as $thread_id) {
+          Log::info('$thread_id', [$thread_id]);
+          foreach ($threads_in_this_category as $thread_in_this_category) {
+            Log::info('$thread_in_this_category', [$thread_in_this_category]);
+            if ($thread_id == $thread_in_this_category->id) {
+              $recently_commented_thread_ids_in_this_category[] = $thread_id;
+              break;
+            }
           }
         }
-      }
-      Log::info('$recently_commented_thread_ids_in_this_category[]', $recently_commented_thread_ids_in_this_category);
+        Log::info('$recently_commented_thread_ids_in_this_category[]', $recently_commented_thread_ids_in_this_category);
 
-      $recently_commented_threads = Thread::whereIn('id', $recently_commented_thread_ids_in_this_category)->orderByRaw('FIELD(id, ' . implode(',', $recently_commented_thread_ids_in_this_category) . ')')->take(10)->get();
-      Log::info('$recently_commented_threads', [$recently_commented_threads]);
+        $recently_commented_threads = Thread::whereIn('id', $recently_commented_thread_ids_in_this_category)->orderByRaw('FIELD(id, ' . implode(',', $recently_commented_thread_ids_in_this_category) . ')')->take(10)->get();
+        Log::info('$recently_commented_threads', [$recently_commented_threads]);
+      }
     }
 
     return view('category.show', compact('category', 'categories', 'recently_commented_threads'));
